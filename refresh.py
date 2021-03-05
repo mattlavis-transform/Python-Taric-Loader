acbe1cceeb2524b1e1c7a5e9f3ba07b7b8fdf3e1
Matt Lavis <matthew.lavis@gmail.com>
2021-03-05 09:05:48 +0000

    Refresh materialized view

---
+++ refresh.py
@@ -0,0 +1,17 @@
+import sys
+import common.globals as g
+from common.database import Database
+
+if len(sys.argv) > 1:
+    g.app.DBASE = sys.argv[1]
+    if g.app.DBASE == "xi":
+        g.app.DBASE = "tariff_xi_production"
+    elif g.app.DBASE == "uk":
+        g.app.DBASE = "tariff_uk_production"
+else:
+    print("Define the database into which to import the Taric XML")
+    sys.exit()
+
+sql = "REFRESH MATERIALIZED VIEW utils.materialized_measures_real_end_dates"
+d = Database()
+d.run_query(sql)
